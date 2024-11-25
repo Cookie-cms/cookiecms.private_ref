6b6d8ae96f9d5be1a3c1679be4d6c0d35ce775d2
coffee <coffeedevloper.misha@gmail.com>
2024-11-25 19:35:58 +0100

    patch

--- src/modules/debug/logs.php
+++ src/modules/debug/logs.php
@@ -1,5 +1,15 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");  // You can replace '*' with specific domain if needed
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// If it's a pre-flight request, return immediately
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 
 $logFile = $_SERVER['DOCUMENT_ROOT'] . '/logs/app.log';
 if (file_exists($logFile)) {
