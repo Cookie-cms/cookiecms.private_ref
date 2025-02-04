d0d5f9d471214f2793fff8e5c230c456af34cbfc
coffee <coffeedevloper.misha@gmail.com>
2025-02-04 14:59:34 +0100

    add license

--- src/inc/DiscordModule.php
+++ src/inc/DiscordModule.php
@@ -1,10 +1,18 @@
 <?php
-/* Discord Oauth v.4.1
- * This file contains the core functions of the oauth2 script.
- * @author : MarkisDev
- * @copyright : https://markis.dev
- */
-
+# This file is part of CookieCms.
+#
+# CookieCms is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CookieCms is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with CookieCms. If not, see <http://www.gnu.org/licenses/>.
 # Starting the script without session (no session required)
 // require_once $_SERVER['DOCUMENT_ROOT'] . "/src/define.php";
 // Заменить $file_path на $configPath
